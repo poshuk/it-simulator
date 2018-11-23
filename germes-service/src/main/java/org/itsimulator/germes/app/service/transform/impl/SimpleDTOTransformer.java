a09f8e0c1bc7f0f32f413fc944711f15d69e71d1
poshuk <poshuk2009@gmail.com>
2018-11-23 16:54:15 +0200

    Logger has been added

--- germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
@@ -1,18 +1,28 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.itsimulator.germes.app.infra.util.Checks;
 import org.itsimulator.germes.app.infra.util.ReflectionUtil;
 import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
 import org.itsimulator.germes.app.rest.dto.base.BaseDTO;
 import org.itsimulator.germes.app.service.transform.Transformer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleDTOTransformer implements Transformer{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
+
     @Override
     public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz){
         checkParams(entity, clz);
         P dto = ReflectionUtil.createInstance(clz);
         ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarFields(entity.getClass(), clz));
         dto.transform(entity);
+        if (LOGGER.isDebugEnabled()){
+            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO Object", ReflectionToStringBuilder.toString(dto, ToStringStyle.SHORT_PREFIX_STYLE));
+        }
         return dto;
     }
 

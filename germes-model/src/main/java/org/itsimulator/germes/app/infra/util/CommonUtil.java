4efcd5309f3ace76878cbd7fc4aabe02124fca64
poshuk <poshuk2009@gmail.com>
2018-11-28 09:07:06 +0200

    The toString utility method has been added

--- germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
+++ germes-model/src/main/java/org/itsimulator/germes/app/infra/util/CommonUtil.java
@@ -1,5 +1,8 @@
 package org.itsimulator.germes.app.infra.util;
 
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -31,5 +34,9 @@ public class CommonUtil {
 	public static <T> List<T> getSafeList(List<T> source) {
 		return Collections.unmodifiableList(Optional.ofNullable(source).orElse(Collections.emptyList()));
 	}
+
+	public static String toString(Object param){
+	    return ReflectionToStringBuilder.toString(param, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
 	
 }

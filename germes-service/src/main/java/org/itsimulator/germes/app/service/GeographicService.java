2c4b6ca01f0027ad780000ec3fe1a8b72ec02f5f
poshuk <poshuk2009@gmail.com>
2018-11-30 16:27:38 +0200

    Two methods has been added

--- germes-service/src/main/java/org/itsimulator/germes/app/service/GeographicService.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/GeographicService.java
@@ -1,8 +1,12 @@
 package org.itsimulator.germes.app.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.itsimulator.germes.app.model.entity.geography.City;
+import org.itsimulator.germes.app.model.entity.geography.Station;
+import org.itsimulator.germes.app.model.search.criteria.StationCriteria;
+import org.itsimulator.germes.app.model.search.criteria.range.RangeCriteria;
 
 /**
  * Entry point to perform operations
@@ -23,6 +27,11 @@ public interface GeographicService {
 	 * @param city
 	 */
 	void saveCity(City city);
+
+	Optional<City> findCityById(int id);
+	List<Station> searchStations(StationCriteria criteria, RangeCriteria rangeCriteria);
+
+
 	
 
 }

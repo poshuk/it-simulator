d44bd0c7475ccb49cceee2076c987e575b7ab0b6
poshuk <poshuk2009@gmail.com>
2018-12-06 17:53:27 +0200

    REST web-service that handles city-related requests

--- germes-service/src/main/java/org/itsimulator/germes/app/rest/service/CityResource.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/service/CityResource.java
@@ -1,13 +1,22 @@
 package org.itsimulator.germes.app.rest.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import jersey.repackaged.com.google.common.collect.Lists;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.itsimulator.germes.app.model.entity.geography.City;
+import org.itsimulator.germes.app.model.entity.transport.TransportType;
+import org.itsimulator.germes.app.rest.dto.CityDTO;
+import org.itsimulator.germes.app.rest.service.base.BaseResource;
+import org.itsimulator.germes.app.service.GeographicService;
+import org.itsimulator.germes.app.service.impl.GeographicServiceImpl;
+import org.itsimulator.germes.app.service.transform.Transformer;
+import org.itsimulator.germes.app.service.transform.impl.SimpleDTOTransformer;
 
 @Path("cities")
 /**
@@ -15,11 +24,45 @@ import jersey.repackaged.com.google.common.collect.Lists;
  * @author Morenets
  *
  */
-public class CityResource {
+public class CityResource extends BaseResource{
+    private final GeographicService service;
+    private final Transformer transformer;
+    public CityResource(){
+        transformer = new SimpleDTOTransformer();
+        service = new GeographicServiceImpl();
+        City city = new City("Odessa");
+        city.addStation(TransportType.AUTO);
+        service.saveCity(city);
+    }
+
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<String> findCities() {
-		return Lists.newArrayList("Odessa", "Kiyv");
+	public List<CityDTO> findCities() {
+		return service.findCities().stream().map((city) -> transformer.transform(city, CityDTO.class)).collect(Collectors.toList());
 	}
+
+	@POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void saveCity(CityDTO cityDTO){
+        service.saveCity(transformer.untransform(cityDTO, City.class));
+    }
+
+    @Path("/{cityId}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findCityById(@PathParam("cityId") final String cityId){
+        if (!NumberUtils.isCreatable(cityId)){
+            return BAD_REQUEST;
+        }
+
+        Optional<City> city = service.findCityById(NumberUtils.toInt(cityId));
+        if (!city.isPresent()){
+            return NOT_FOUND;
+        }
+
+        return ok(transformer.transform(city.get(), CityDTO.class));
+    }
+
+
 }
--- germes-service/src/test/java/org/itsimulator/germes/app/rest/service/CityResourceTest.java
+++ germes-service/src/test/java/org/itsimulator/germes/app/rest/service/CityResourceTest.java
@@ -1,12 +1,18 @@
 package org.itsimulator.germes.app.rest.service;
 
 import org.glassfish.jersey.test.JerseyTest;
+import org.itsimulator.germes.app.rest.dto.CityDTO;
 import org.itsimulator.germes.app.rest.service.config.JerseyConfig;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -18,9 +24,47 @@ public class CityResourceTest extends JerseyTest {
 
     @Test
     public void testFindCitiesSuccess(){
-        List<?> cities = target("cities").request().get(List.class);
+        List<Map<String, String>> cities = target("cities").request().get(List.class);
+
         assertNotNull(cities);
-        assertTrue(cities.contains("Odessa"));
-        assertTrue(cities.contains("Kiyv"));
+        assertEquals(cities.size(), 1);
+
+        Map<String, String> city = cities.get(0);
+        assertEquals(city.get("name"), "Odessa");
     }
+
+    @Test
+    public void testFindCityByIdSuccess(){
+        CityDTO city = target("cities/1").request().get(CityDTO.class);
+        assertNotNull(city);
+        assertEquals(city.getId(), 1);
+        assertEquals(city.getName(), "Odessa");
+    }
+
+    @Test
+    public void testFindCityByIdNotFound(){
+        Response response = target("cities/2").request().get(Response.class);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    public void testFindCityByIdInvalid(){
+        Response response = target("cities/aaab").request().get(Response.class);
+        assertNotNull(response);
+        assertEquals(response.getStatus(), Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
+
+    @Test
+    public void testSaveCitySuccess(){
+        CityDTO city = new CityDTO();
+        city.setName("Kiev");
+
+        Response response = target("cities").request().post(Entity.entity(city, MediaType.APPLICATION_JSON));
+        assertEquals(response.getStatus(), Response.Status.NO_CONTENT.getStatusCode());
+    }
+
+
+
 }

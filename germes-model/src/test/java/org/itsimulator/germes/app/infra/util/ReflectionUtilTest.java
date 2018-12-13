582b72205d678263ad2d262cb9ff994f74cf75b1
poshuk <poshuk2009@gmail.com>
2018-12-13 12:38:13 +0200

    Added tests

--- germes-model/src/test/java/org/itsimulator/germes/app/infra/util/ReflectionUtilTest.java
+++ germes-model/src/test/java/org/itsimulator/germes/app/infra/util/ReflectionUtilTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.itsimulator.germes.app.infra.exception.ConfigurationException;
 import org.itsimulator.germes.app.infra.exception.flow.InvalidParameterException;
+import org.itsimulator.germes.app.infra.util.annotation.Ignore;
 import org.junit.Test;
 
 /**
@@ -23,12 +24,12 @@ public class ReflectionUtilTest {
 	}
 
 	@Test(expected = ConfigurationException.class)
-	public void createInstanceFailure() {
+	public void testCreateInstanceFailure() {
 		ReflectionUtil.createInstance(Restricted.class);
 	}
 
 	@Test
-	public void findSimilarFieldsSuccess() {
+	public void testFindSimilarFieldsSuccess() {
 		List<String> fields = ReflectionUtil.findSimilarFields(Source.class,
 				Destination.class);
 		assertNotNull(fields);
@@ -36,7 +37,7 @@ public class ReflectionUtilTest {
 	}
 
 	@Test
-	public void copyFieldsSuccess() {
+	public void testCopyFieldsSuccess() {
 		Source src = new Source();
 		src.setValue(10);
 		Destination dest = new Destination();
@@ -45,7 +46,26 @@ public class ReflectionUtilTest {
 		ReflectionUtil.copyFields(src, dest, fields);
 		assertEquals(dest.getValue(), 10);
 	}
+	
+	@Test
+	public void copyFindSimilarFieldsWithIgnoreSuccess() {
+		List<String> fields = ReflectionUtil.findSimilarFields(Source.class, Destination.class);
+		assertFalse(fields.contains("ignored"));
+	}
 
+	@Test
+	public void copyFindSimilarFieldsForStaticAndFinalSuccess() {
+		List<String> fields = ReflectionUtil.findSimilarFields(Source.class, Destination.class);
+		assertFalse(fields.contains("staticField"));
+		assertFalse(fields.contains("finalField"));
+	}
+
+	@Test
+	public void copyFindSimilarFieldsForBaseFieldSuccess() {
+		List<String> fields = ReflectionUtil.findSimilarFields(Source.class, Destination.class);
+		assertTrue(fields.contains("baseField"));
+	}
+	
 	@Test(expected=InvalidParameterException.class)
 	public void copyFieldsDestinationNullFailure() {
 		Source src = new Source();
@@ -53,18 +73,39 @@ public class ReflectionUtilTest {
 	}	
 }
 
-class Source {
+class BaseSource {
+	private int baseField;
+}
+
+class BaseDestination {
+	private int baseField;
+}
+
+class Source extends BaseSource {
 	private int value;
 
 	private String text;
+	
+	@Ignore
+	private int ignored = 2;
+	
+	private static int staticField;
+	
+	private final int finalField = 0;
 
 	public void setValue(int value) {
 		this.value = value;
 	}
 }
 
-class Destination {
+class Destination extends BaseDestination {
 	private int value;
+	
+	private int ignored;
+	
+	private int staticField;
+	
+	private int finalField = 0;	
 
 	public int getValue() {
 		return value;
--- germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/transform/impl/SimpleDTOTransformer.java
@@ -1,7 +1,5 @@
 package org.itsimulator.germes.app.service.transform.impl;
 
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.itsimulator.germes.app.infra.util.Checks;
 import org.itsimulator.germes.app.infra.util.CommonUtil;
 import org.itsimulator.germes.app.infra.util.ReflectionUtil;
@@ -11,37 +9,58 @@ import org.itsimulator.germes.app.service.transform.Transformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SimpleDTOTransformer implements Transformer{
+/**
+ * Default transformation engine that uses reflection to transform objects
+ * 
+ * @author Morenets
+ *
+ */
+public class SimpleDTOTransformer implements Transformer {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(SimpleDTOTransformer.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleDTOTransformer.class);
+	@Override
+	public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(
+			final T entity, final Class<P> clz) {
+		checkParams(entity, clz);
 
-    @Override
-    public <T extends AbstractEntity, P extends BaseDTO<T>> P transform(final T entity, final Class<P> clz){
-        checkParams(entity, clz);
-        P dto = ReflectionUtil.createInstance(clz);
-        ReflectionUtil.copyFields(entity, dto, ReflectionUtil.findSimilarFields(entity.getClass(), clz));
-        dto.transform(entity);
-        if (LOGGER.isDebugEnabled()){
-            LOGGER.debug("SimpleDTOTransformer.transform: {} DTO Object", CommonUtil.toString(dto));
-        }
-        return dto;
-    }
+		P dto = ReflectionUtil.createInstance(clz);
+		// Now just copy all the similar fields
+		ReflectionUtil.copyFields(entity, dto,
+				ReflectionUtil.findSimilarFields(entity.getClass(), clz));
+		dto.transform(entity);
 
-    private void checkParams(final Object param, final Class<?> clz){
-        Checks.checkParameter(param!=null, "Source transformation object is not initialized");
-        Checks.checkParameter(clz!=null, "No class is defined for transformation");
-    }
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("SimpleDTOTransformer.transform: {} DTO object",
+					CommonUtil.toString(dto));
+		}
 
-    @Override
-    public <T extends AbstractEntity, P extends BaseDTO<T>> T untransform(final P dto, final Class<T> clz){
-        checkParams(dto, clz);
-        T entity = ReflectionUtil.createInstance(clz);
-        ReflectionUtil.copyFields(dto, entity, ReflectionUtil.findSimilarFields(dto.getClass(), clz));
-        dto.untransform(entity);
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("SimpleDTOTransformer.transform: {} entity",
-                    CommonUtil.toString(dto));
-        }
-        return entity;
-    }
+		return dto;
+	}
+
+	private void checkParams(final Object param, final Class<?> clz) {
+		Checks.checkParameter(param != null,
+				"Source transformation object is not initialized");
+		Checks.checkParameter(clz != null,
+				"No class is defined for transformation");
+	}
+
+	@Override
+	public <T extends AbstractEntity, P extends BaseDTO<T>> T untransform(
+			final P dto, final Class<T> clz) {
+		checkParams(dto, clz);
+
+		T entity = ReflectionUtil.createInstance(clz);
+
+		ReflectionUtil.copyFields(dto, entity,
+				ReflectionUtil.findSimilarFields(dto.getClass(), clz));
+		dto.untransform(entity);
+
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("SimpleDTOTransformer.transform: {} entity",
+					CommonUtil.toString(dto));
+		}
+
+		return entity;
+	}
 }

c61c5de62023215ef1798a03e64551583cdbd22e
poshuk <poshuk2009@gmail.com>
2019-01-18 23:49:53 +0200

    Default constructor and annotation has been added

--- germes-model/src/main/java/org/itsimulator/germes/app/model/entity/geography/City.java
+++ germes-model/src/main/java/org/itsimulator/germes/app/model/entity/geography/City.java
@@ -4,39 +4,46 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import org.itsimulator.germes.app.infra.util.CommonUtil;
 import org.itsimulator.germes.app.model.entity.base.AbstractEntity;
 import org.itsimulator.germes.app.model.entity.transport.TransportType;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.FetchType;
-import javax.persistence.OneToMany;
-
 /**
  * Any locality that contains transport stations
+ * 
  * @author Morenets
  *
  */
-public class City extends AbstractEntity {	
+@Table(name = "CITY")
+@Entity
+public class City extends AbstractEntity {
 	private String name;
-	
+
 	/**
 	 * Name of the district where city is placed
 	 */
 	private String district;
-	
+
 	/**
-	 * Name of the region where district is located.
-	 * Region is top-level area in the country
+	 * Name of the region where district is located. Region is top-level area in
+	 * the country
 	 */
 	private String region;
-	
+
 	/**
-	 * Set of transport stations that is linked to this 
-	 * locality
+	 * Set of transport stations that is linked to this locality
 	 */
 	private Set<Station> stations;
+
+	public City() {
+	}
 
 	public City(final String name) {
 		this.name = name;
@@ -51,6 +58,7 @@ public class City extends AbstractEntity {
 		this.name = name;
 	}
 
+	@Column(name = "DISTRICT", nullable = false, length = 32)
 	public String getDistrict() {
 		return district;
 	}
@@ -68,8 +76,8 @@ public class City extends AbstractEntity {
 		this.region = region;
 	}
 
-	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "city", orphanRemoval = true)
-	public Set<Station> getStations() {		
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "city", orphanRemoval = true)	
+	public Set<Station> getStations() {
 		return CommonUtil.getSafeSet(stations);
 	}
 
@@ -79,25 +87,27 @@ public class City extends AbstractEntity {
 
 	/**
 	 * Adds specified station to the city station list
+	 * 
 	 * @param station
 	 */
 	public Station addStation(final TransportType transportType) {
-		if(stations == null) {
+		if (stations == null) {
 			stations = new HashSet<>();
 		}
 		Station station = new Station(this, transportType);
 		stations.add(station);
-		
+
 		return station;
 	}
 
 	/**
 	 * Removes specified station from city station list
+	 * 
 	 * @param station
 	 */
 	public void removeStation(Station station) {
 		Objects.requireNonNull(station, "station parameter is not initialized");
-		if(stations == null) {
+		if (stations == null) {
 			return;
 		}
 		stations.remove(station);

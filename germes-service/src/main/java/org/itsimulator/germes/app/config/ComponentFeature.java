ce28acf970d23a291c8341df6123b182d8236848
poshuk <poshuk2009@gmail.com>
2019-01-18 23:18:47 +0200

    ComponentFeature replaced by ComponentBinder

--- germes-service/src/main/java/org/itsimulator/germes/app/config/ComponentFeature.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/config/ComponentFeature.java
@@ -3,10 +3,16 @@ package org.itsimulator.germes.app.config;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 
+/**
+ * Registers DI bindings
+ * @author Morenets
+ *
+ */
 public class ComponentFeature implements Feature {
+
     @Override
-    public boolean configure(final FeatureContext context){
-        context.register(new ComponentFeature());
+    public boolean configure(final FeatureContext context) {
+        context.register(new ComponentBinder());
         return true;
     }
 }

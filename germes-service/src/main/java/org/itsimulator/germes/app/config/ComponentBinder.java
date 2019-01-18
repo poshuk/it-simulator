46cf346dd6e8c6bd620a87fef839f21180c6f1f5
poshuk <poshuk2009@gmail.com>
2019-01-18 23:15:09 +0200

    InMemoryCityRepository replaced by HibernateCityRepository

--- germes-service/src/main/java/org/itsimulator/germes/app/config/ComponentBinder.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/config/ComponentBinder.java
@@ -1,23 +1,27 @@
 package org.itsimulator.germes.app.config;
 
+import javax.inject.Singleton;
+
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.itsimulator.germes.app.persistence.hibernate.SessionFactoryBuilder;
 import org.itsimulator.germes.app.persistence.repository.CityRepository;
-import org.itsimulator.germes.app.persistence.repository.inmemory.InMemoryCityRepository;
+import org.itsimulator.germes.app.persistence.repository.hibernate.HibernateCityRepository;
 import org.itsimulator.germes.app.service.GeographicService;
 import org.itsimulator.germes.app.service.impl.GeographicServiceImpl;
 import org.itsimulator.germes.app.service.transform.Transformer;
 import org.itsimulator.germes.app.service.transform.impl.SimpleDTOTransformer;
 
-import javax.inject.Singleton;
-
-public class ComponentBinder extends AbstractBinder{
+/**
+ * Binds bean implementations and implemented interfaces
+ * @author Morenets
+ *
+ */
+public class ComponentBinder extends AbstractBinder {
     @Override
-    protected void configure(){
-        bind(InMemoryCityRepository.class).to(CityRepository.class).in(Singleton.class);
+    protected void configure() {
+        bind(HibernateCityRepository.class).to(CityRepository.class).in(Singleton.class);
         bind(SimpleDTOTransformer.class).to(Transformer.class).in(Singleton.class);
         bind(GeographicServiceImpl.class).to(GeographicService.class).in(Singleton.class);
-		bind(SessionFactoryBuilder.class).to(SessionFactoryBuilder.class).in(Singleton.class);
+        bind(SessionFactoryBuilder.class).to(SessionFactoryBuilder.class).in(Singleton.class);
     }
-
 }
